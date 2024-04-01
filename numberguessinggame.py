95edafc90bf9b9232f52818add7cf8b3b91aa764
olevy100 <165201284+olevy100@users.noreply.github.com>
2024-03-31 17:11:47 -0700

    numberguessinggame.py

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -2,7 +2,7 @@ import numpy as np
 x = np.random.choice(range(1,100))
 print("This is a number guessing game, you will get 5 chances to guess a random number from 1 - 100.")
 print(" ")
-for x in range(5):
+for o in range(1, 100):
     a = int(input("what is your guess?"))
     if a == x:
         print(f"congrats, you got it, {x} was the correct number!")
@@ -11,4 +11,4 @@ for x in range(5):
         print(f"sorry, {a} is too big, try again")
     elif a < x:
         print(f"sorry, {a} is too small, try again")
-print("sorry, you didn't get it, try again sometime.")
+print(f"{x} was the number, sorry, you didn't get it, try again sometime.")
