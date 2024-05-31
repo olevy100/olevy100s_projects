f92563601f4a488e8a4662c041d005b23313fa4d
olevy100 <165201284+olevy100@users.noreply.github.com>
2024-05-31 15:04:16 -0700

    madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,29 @@
+#may return EOFError: EOF when reading a line, I don't know why.
+import random
+pluralnoun1 = input("Enter a plural noun:")
+occupation = input("Enter an occupation:")
+place1 = input("Enter a place:")
+number = input("Enter a number:")
+adjective1 = input("Enter an adjective:")
+ing_verb1 = input("Enter a verb ending with 'ing':")
+pluralnoun2 = input("Enter a plural noun:")
+place2 = input("Enter a place:")
+adjective2 = input("Enter an adjective:")
+pluralnoun3 = input("Enter a plural noun:")
+ing_verb2 = input("Enter verb ending with 'ing':")
+pluralnoun4 = input("Enter plural noun:")
+adjecive3 = input("Enter an adjective:")
+noun1 = input("Enter a noun:")
+bodypart1 = input("Enter a body part:")
+verb1 = input("Enter a verb:")
+adjective4 = input("Enter an adjective:")
+bodypart2 = input("Enter a body part:")
+print(f"""Hello my fellow {pluralnoun1}, it's me, George Washington, the first {occupation} 
+I am writing from (the) {place1},where I have been living for the past {number} years. I am concerned 
+by the {adjective1} state of affairs in America these days. It seems your politicians are more concerned with 
+{ing_verb1} one another than with listening to the {pluralnoun2} of the people. When declared our independance from
+(the) {place2},  we set forth on a/an {adjective2} path guided by the everyday {pluralnoun3}. If we're going 
+to keep {ing_verb2}, then we're going to have to learn how to respect all {pluralnoun4}. Don't get me wrong; we had {adjecive3} problems 
+in my day, too. Benjamin Franklin once called me a/an {noun1} and kicked me in the {bodypart1}. But at 
+the end of the day, we were able to {verb1} in harmony. Let us find that spirit once again, or
+else I'm taking my {bodypart2} off the quarter""")
