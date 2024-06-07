1f19433bd6a515a4a4d3a78509ff0b7a5d52f313
olevy100 <165201284+olevy100@users.noreply.github.com>
2024-06-07 14:41:33 -0700

    Does not always choose 1

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -1,14 +1,15 @@
-import numpy as np
-x = np.random.choice(range(1,100))
-print("This is a number guessing game, you will get 5 chances to guess a random number from 1 - 100.")
+import random
+secret_number = random.choice(range(1, 101))
+print("""This is a number guessing game, you will get 5 chances to guess a random number
+from 1 - 100.""")
 print(" ")
-for o in range(1, 100):
+for x in range(5):
     a = int(input("what is your guess?"))
-    if a == x:
+    if a == secret_number:
         print(f"congrats, you got it, {x} was the correct number!")
         quit()
-    elif a > x:
-        print(f"sorry, {a} is too big, try again")
-    elif a < x:
+    elif a < secret_number:
         print(f"sorry, {a} is too small, try again")
-print(f"{x} was the number, sorry, you didn't get it, try again sometime.")
+    elif a > secret_number:
+        print(f"sorry, {a} is too big, try again")
+print("sorry, you didn't get it, try again sometime.")
